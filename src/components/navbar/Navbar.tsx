72b55d9f8070aaa4f4fee10dac79a654bdaf54bc
Nathan <ribeiro.nathan2002@gmail.com>
2025-02-20 12:17:03 -0300

    Implementado CRUD de postagens e aplicada estilização na página Home.

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom"
 import { AuthContext } from "../../contexts/AuthContext";
+import logo from "../../assets/logo.png";
 
 function Navbar() {
 
@@ -17,16 +18,18 @@ function Navbar() {
     
     return (
         <>
-            <div className='w-full bg-indigo-900 text-white
-                flex justify-center py-4'>
-
+            <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
                 <div className="container flex justify-between text-lg">
-                    <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
+                   {/* Adicionando a logo */}
+                    <div className="container flex items-center gap-2">
+                        <img src={logo} alt="Logo" className="h-10" />
+                        <Link to='/home' className="text-2xl font-bold">Nathan Ribeiro</Link>
+                    </div>
 
-                    <div className='flex gap-4'>
-                        Postagens
+                    <div className='flex items-center gap-6'>
+                        <Link to='/postagens' className='hover:underline'>Postagens</Link>
                         <Link to='/temas' className='hover:underline'>Temas</Link>
-                        <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link>
+                        <Link to='/cadastrartema' className='hover:underline whitespace-nowrap'>Cadastrar tema</Link>
                         Perfil
                         <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
                     </div>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,37 +1,54 @@
+import ListaPostagens from "../../components/postagens/listapostagens/ListaPostagens"
+import ModalPostagem from "../../components/postagens/modalpostagem/ModalPostagem"
+import homeImage from "../../assets/home.png";
 
 function Home() {
-  return (
-      <>
-          <div className="bg-indigo-900 flex justify-center">
-              <div className='container grid grid-cols-2 text-white'>
-                  <div className="flex flex-col gap-4 items-center justify-center py-4">
-                      <h2 className='text-5xl font-bold'>
-                          Seja Bem Vindo!
-                      </h2>
-                      <p className='text-xl'>
-                          Expresse aqui seus pensamentos e opniões
-                      </p>
 
-                      <div className="flex justify-around gap-4">
-                          <div className='rounded text-white 
-                                          border-white border-solid border-2 py-2 px-4'
-                              >
-                              Nova Postagem
-                          </div>
-                      </div>
-                  </div>
+    return (
+        <>
+            <div className="bg-blue-900 flex justify-center">
+                <div className='container grid grid-cols-2 text-white'>
+                    <div className="flex flex-col gap-4 items-center justify-center py-4">
+                        <h2 className='text-5xl font-bold'>
+                            Seja Bem Vindo!
+                        </h2>
+                        <p className='text-xl'>
+                            Expresse aqui seus pensamentos e opniões
+                        </p>
 
-                  <div className="flex justify-center ">
-                      <img
-                          src="https://i.imgur.com/fyfri1v.png"
-                          alt="Imagem Página Home"
-                          className='w-2/3'
-                      />
-                  </div>
-              </div>
-          </div>
-      </>
-  )
+                        <div className="flex justify-around gap-4">                         
+                            <ModalPostagem />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="flex justify-center ">
+                    <img
+                        src={homeImage}
+                        alt="Imagem Página Home"
+                        className='w-2/3'
+                    />
+                </div>
+            </div>
+            
+            {/* Container que ocupa toda a largura da tela */}
+            <div className="relative w-full h-[500px] flex justify-center items-center">
+                {/* Imagem de fundo cobrindo toda a área */}
+                <img
+                    src="https://static.vecteezy.com/ti/vetor-gratis/p1/3809954-digital-binario-codigo-dados-fundo-computador-numeros-conceito-tecnologico-vetor.jpg"
+                    alt="Imagem Fundo"
+                    className="absolute top-0 left-0 w-full h-full object-cover"
+                />
+
+                    {/* ListaPostagens sobrepondo a imagem */}
+                <div className="relative z-10">
+                    <ListaPostagens />
+                </div>
+            </div>        
+        </>
+    )
 }
 
 export default Home
+
+// https://i.imgur.com/fyfri1v.png
